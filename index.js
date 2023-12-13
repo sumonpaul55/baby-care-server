ca21fd7f6ba38af016fe984f1a2d04e885580fd2
sumon paul <sumpaul100@gmail.com>
2023-12-13 19:33:46 +0600

    link updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // add middleware
 // /
 app.use(cors({
-    // origin: ["http://localhost:5173"],// ["https://littlestars-care.web.app"],
+    // origin: ["http://localhost:5173", "http://facebook.com"],// ["https://littlestars-care.web.app"],
     origin: ["https://littlestars-care.web.app"],// ["https://littlestars-care.web.app"],
     credentials: true
 }))
