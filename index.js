8a450dfebf4c4cf55e00783cdcbd4ddff91091c2
sumon paul <sumpaul100@gmail.com>
2023-12-13 19:43:14 +0600

    facebook plugins link added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // /
 app.use(cors({
     // origin: ["http://localhost:5173", "http://facebook.com"],// ["https://littlestars-care.web.app"],
-    origin: ["https://littlestars-care.web.app", "http://facebook.com"],// ["https://littlestars-care.web.app"],
+    origin: ["https://littlestars-care.web.app", "http://facebook.com", "https://web.facebook.com/plugins/customer_chat/SDK/?app_id=2139840976393717&channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df8bd7f287c252%26domain%3Dlittlestars-care.web.app%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Flittlestars-care.web.app%252Ff3bd438ceb8147%26relation%3Dparent.parent&current_url=https%3A%2F%2Flittlestars-care.web.app%2F&event_name=chat_plugin_sdk_facade_create&is_loaded_by_facade=true&loading_time=0&locale=en_US&log_id=60b29fe7-c638-43aa-b1aa-7bf6db7bd924&page_id=1744153689139754&request_time=1702474897102&sdk=joey&should_use_new_domain=false&suppress_http_code=1"],// ["https://littlestars-care.web.app"],
     credentials: true
 }))
 app.use(cookieParser())
